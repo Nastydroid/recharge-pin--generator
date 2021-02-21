77b7b392d4b4b224dd243f0377d7b5f2a36a917d
Nastydroid <icekidz1997@gmail.com>
2021-02-21 17:44:09 +0800

    first commit

---
+++ generate random pin.php
@@ -0,0 +1,9 @@
+<?php
+echo "Airtime recharge pin for sidehustle telecommunucation<br>";
+$num=1;
+while($num <= 200){
+    echo (rand()) . '<br>';
+    $num++;
+}
+
+?>
---
+++ sidehustle.php
@@ -0,0 +1,22 @@
+<?php
+echo'i am oluwafemi';
+$name='oluwafemi';
+echo $name .'<br>';
+
+$integer = 123456543;
+echo $integer . '<br>';
+
+$floater =3454.2432;
+echo $floater . '<br>';
+
+$names=array('sopade', 'oluwafemi', 'tobi');
+
+print_r($names) .'<br>';
+
+function calc($num1 , $num2){
+    return $num1 * $num2 .'<br>';
+}
+
+echo calc(10,2);
+
+?>
