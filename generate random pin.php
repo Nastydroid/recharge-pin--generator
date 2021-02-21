ffb2a67bb7905df022f1204c19c35db731fc42ac
Oluwafemi <Icekidz1997@gmail.com>
2021-02-21 18:17:41 +0800

    Update generate random pin.php

--- generate random pin.php
+++ generate random pin.php
@@ -1,5 +1,5 @@
 <?php
-echo "Airtime recharge pin for sidehustle telecommunucation<br>";
+echo "Airtime recharge pin for sidehustle telecommunication<br>";
 $num=1;
 while($num <= 200){
     echo (rand()) . '<br>';
